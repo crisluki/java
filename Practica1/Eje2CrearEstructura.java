1953edfe50bb5a5efed1d9597d05f0c18b16a54c
aml360 <aml360esp@gmail.com>
2021-11-23 02:08:03 +0100

    refactor: refactor some exercises

--- Practica1/Eje2CrearEstructura.java
+++ Practica1/Eje2CrearEstructura.java
@@ -2,41 +2,40 @@ package Practica1;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Eje2CrearEstructura {
-	public static void main(String[] args) {
-	String ruta = "C:\\Users\\crisluki\\eclipse-workspace\\AD\\1Trimestre\\";
-	String nombreFichero1 = "P1.txt";
-	String nombreFichero2 = "UD1.txt";
-	String nombreFichero3 = "UD2.txt";
-	String nombreDirectorio1 = "AD";
-	String nombreDirectorio2 = "PSP";
 
-	File f1 = new File(ruta+"\\AD\\"+nombreFichero1);
-	File f2 = new File(ruta+"\\AD\\"+nombreFichero2);
-	File f3 = new File(ruta+"\\AD\\"+nombreFichero3);
-	
-	File f4 = new File(ruta+"\\PSP\\"+nombreFichero2);
-	File f5 = new File(ruta+"\\PSP\\"+nombreFichero3);
-	
-	File f6 = new File(ruta+nombreDirectorio1);
-	File f7 = new File(ruta+nombreDirectorio2);
-	
-	try {
-		f6.mkdir();
-		f1.createNewFile();
-		f2.createNewFile();
-		f3.createNewFile();
-		f7.mkdir();
-		f4.createNewFile();
-		f5.createNewFile();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	if((f1.exists()&&(f2.exists()&&(f3.exists()&&(f4.exists()&&(f5.exists()&&(f6.exists()&&(f7.exists()))))))))
-		{
-		System.out.println("existen los archivos");
+	static String ruta = "C:\\Users\\crisluki\\eclipse-workspace\\AD\\1Trimestre\\";
+	static String nombreFichero1 = "P1.txt";
+	static String nombreFichero2 = "UD1.txt";
+	static String nombreFichero3 = "UD2.txt";
+	static String nombreDirectorio1 = "AD";
+	static String nombreDirectorio2 = "PSP";
+
+	public static void main(String[] args) {
+		var listaArchivos = new ArrayList<File>();
+
+		listaArchivos.add(new File(ruta + "\\AD\\" + nombreFichero1));
+		listaArchivos.add(new File(ruta + "\\AD\\" + nombreFichero2));
+		listaArchivos.add(new File(ruta + "\\AD\\" + nombreFichero3));
+		listaArchivos.add(new File(ruta + "\\PSP\\" + nombreFichero2));
+		listaArchivos.add(new File(ruta + "\\PSP\\" + nombreFichero3));
+		listaArchivos.add(new File(ruta + nombreDirectorio1));
+		listaArchivos.add(new File(ruta + nombreDirectorio2));
+
+		for (var i = 0; i < listaArchivos.size(); i++) {
+			var archivo = listaArchivos.get(i);
+			if (i == 6 || i == 7) {
+				archivo.mkdir();
+			} else {
+				try {
+					archivo.createNewFile();
+				} catch (IOException e) {
+					System.out.println("El archivo " + i + " ya estaba creado");
+				}
+			}
 		}
+		System.out.println("existen los archivos");
 	}
 }
--- Practica1/Eje3RenombrarArchivo.java
+++ Practica1/Eje3RenombrarArchivo.java
@@ -6,15 +6,14 @@ public class Eje3RenombrarArchivo {
 	public static void main(String[] args) {
 		String ruta = "C:\\Users\\crisluki\\eclipse-workspace\\AD\\1Trimestre\\AD\\";
 		String nombreFichero1 = "P1.txt";
-		File f = new File(ruta+nombreFichero1);
-		if(f.exists()){
-			System.out.println("Renombrando el archivo P1.txt -> practica1.txt");
-			String nombreFNuevo = "practica1.txt";
-			File f2 = new File(ruta+nombreFNuevo);
-			f.renameTo(f2);
-			System.out.println("Ya ha sido cambiado");
-		}else {
+		File f = new File(ruta + nombreFichero1);
+		if (!f.exists()) {
 			System.out.println("El archivo no existe");
 		}
+		System.out.println("Renombrando el archivo P1.txt -> practica1.txt");
+		String nombreFNuevo = "practica1.txt";
+		File f2 = new File(ruta + nombreFNuevo);
+		String msg = f.renameTo(f2) ? "Cambiado satisfactoriamente" : "Ya ha sido cambiado";
+		System.out.println(msg);
 	}
 }
